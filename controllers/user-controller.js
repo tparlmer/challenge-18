b6cadda2f897cb864b9d5204c3107f4ef650fad6
tparlmer <tparlmer@gmail.com>
2022-06-13 12:25:17 -0400

    updated user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,40 +3,40 @@ const db = require("../models/index.js");
 const userController = {
     getUsers: async function (req, res) {
         try {
-          const thoughts = await db.Thought.find ();
-          res.status(200).json({ thoughts });
+          const users = await db.User.find ();
+          res.status(200).json({ users });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
       },
     getUserById: async function (req, res) {
         try {
-          const thought = await db.Thought.findById(req.params.id);
-          res.status(200).json({ thought });
+          const user = await db.User.findById(req.params.id);
+          res.status(200).json({ user });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
       },
     createUser: async function (req, res) {
         try {
-          const newThought = db.Thought.createOne(req.body);
-          res.status(200).json({ thought: newThought });
+          const newUser = db.User.createOne(req.body);
+          res.status(200).json({ thought: newUser });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
       },
     updateUser: async function (req, res) {
         try {
-          const updatedThought = await db.Thought.findOneAndUpdate({});
-          res.status(200).json({ thought: updatedThought });
+          const updatedUser = await db.User.findOneAndUpdate({});
+          res.status(200).json({ user: updatedUser });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
       },
     deleteUser: async function (req, res) {
         try {
-          const deletedThought = await db.Thought.findOneAndDelete({});
-          res.status(200).json({ thought: deletedThought });
+          const deletedUser = await db.User.findOneAndDelete({});
+          res.status(200).json({ user: deletedUser });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
