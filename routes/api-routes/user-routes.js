a404985deedbdd0162578e159642e5e248f27307
tparlmer <tparlmer@gmail.com>
2022-06-13 12:21:07 -0400

    updated user-routes.js

--- routes/api-routes/user-routes.js
+++ routes/api-routes/user-routes.js
@@ -1,24 +1,33 @@
 const router = require('express').Router();
+const {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controller');
 
 // /api/users
 
-// router.route('/')
-//     .get()
-//     .get()
-//     .post()
-//     .put()
-//     .delete()
-
 // GET all users
-// GET a single user by its _id and populated thought and friend data
 // POST a new user:
 // example data
 // {
 //     "username": "lernantino",
 //     "email": "lernantino@gmail.com"
 //   }
-// PUT to update a user by its _id
-// DELETE to remove user by its _id
+router.route('/')
+    .get(getUsers)
+    .post(createUser)
+    
+    // GET a single user by its _id and populated thought and friend data
+    // PUT to update a user by its _id
+    // DELETE to remove user by its _id
+
+router.route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
 
 
 // /api/users/:userId/friends/:friendId
