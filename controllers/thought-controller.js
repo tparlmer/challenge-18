6d11a98b06af365c53eaeabd2bcfb95134bdf641
tparlmer <tparlmer@gmail.com>
2022-06-14 14:12:58 -0400

    adjusted routes and controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -43,6 +43,22 @@ const thoughtController = {
       res.status(500).json({ error: error.message });
     }
   },
+  createReaction: async function (req, res) {
+    try {
+      const createdReaction = await db.Thought.createOne(req.body);
+      res.status(200).json({ reactions: deletedThought });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+  deleteReaction: async function (req, res) {
+    try {
+      const deletedReaction = await db.Thought.findOneAndDelete({});
+      res.status(200).json({ reactions: deletedThought });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
 };
 
 module.exports = thoughtController;
